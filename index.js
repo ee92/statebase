d24ef6d70b650d53225b82fe373249c49399a38e
egor <eegor650@gmail.com>
2019-06-22 12:08:16 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+function createState(initialState) {
+   var state = initialState;
+   var listeners = {};
+
+   function createRef(key, path = [key]) {
+      
+      function ref(key) {
+         return createRef(key, path.concat(key));
+      }
+
+      function val() {
+         var val = state;
+         for (let i=1; i<path.length; i++) {
+            if (!val[path[i]]) return undefined;
+            val = val[path[i]];
+         }
+         return val;
+      }
+
+      function callListeners() {
+         for (let i=0; i<path.length; i++) {
+            var paths = path.slice(0, i+1);
+            const id = paths.join('-');
+            if (!listeners[id]) continue;
+            for (let j=0; j<listeners[id].length; j++) {
+               var ref = createRef(paths[i], paths);
+               listeners[id][j](ref);
+            }
+         }
+      }
+
+      function set(value) {
+         var update = state;
+         for (var i=1; i<path.length-1; i++) {
+            update = update[path[i]];
+         }
+         if (update[key] === value) return;
+         update[key] = value;
+         callListeners();
+      }
+
+      function listen(listener) {
+         const id = path.join('-');
+         if (!listeners[id]) {
+            listeners[id] = [];
+         }
+         listeners[id].push(listener);
+         return function() {
+            const index = listeners[id].indexOf(listener);
+            listeners[id].splice(index, 1);
+         }
+      }
+
+      return {
+         ref: ref,
+         val: val,
+         set: set,
+         listen: listen
+      }
+   }
+
+   return createRef('state');
+}
+
+module.exports = createState;
---
+++ test.js
@@ -0,0 +1,84 @@
+var createState = require('./index.js');
+
+function checkSetDeep() {
+   var state = createState({});
+   state.ref('x').set({});
+   state.ref('x').ref('y').set('wow');
+   if (state.ref('x').ref('y').val() !== 'wow') {
+      console.warn('set deep not working');
+   }
+}
+
+function checkInitialState() {
+   var state = createState({
+      a: 5,
+      b: true,
+      c: {
+         a: 100,
+         b: {
+            c: 'wow'
+         }
+      }
+   });
+   if (
+      state.ref('a').val() !== 5
+      || state.ref('c').ref('a').val() !== 100
+      || state.ref('c').ref('b').ref('c').val() !== 'wow'
+   ) {
+      console.warn('initialiazed with wrong value');
+   }
+}
+
+function checkListener() {
+   var state = createState({
+      a: 5,
+      b: true,
+      c: {
+         a: 100,
+         b: {
+            c: 'wow'
+         }
+      }
+   });
+   var error = true;
+   const unsub = state.ref('a').listen(function(snap) {
+      error = snap.val() !== 6;
+   })
+   state.ref('a').set(6);
+   unsub();
+   state.ref('a').set(7);
+   if (error) {
+      console.warn('listener no work');
+   }
+}
+
+function checkListenerDeep() {
+   var state = createState({
+      a: 5,
+      b: true,
+      c: {
+         a: 100,
+         b: {
+            c: 'wow'
+         }
+      }
+   });
+   var error = true;
+   const unsub = state.ref('c').listen(function(snap) {
+      error = false;
+   })
+   state.ref('c').ref('a').set(42);
+   unsub();
+   if (error) {
+      console.warn('listener no work on deep change');
+   }
+}
+
+function runTests() {
+   checkInitialState();
+   checkListener();
+   checkListenerDeep();
+   checkSetDeep();
+}
+
+runTests();
